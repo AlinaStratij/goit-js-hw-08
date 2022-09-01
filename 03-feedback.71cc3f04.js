59cc6653b3554795f7ac6f6a64a293bd2bc2fbdf
AlinaStratij <99261910+AlinaStratij@users.noreply.github.com>
2022-09-01 22:54:06 +0000

    Deploying to gh-pages from @ AlinaStratij/goit-js-hw-08@bbeaa3c7fb4dc63d7f6e723c85fad50095464d0a 🚀

---
+++ 03-feedback.71cc3f04.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form textarea"),t=document.querySelector(".feedback-form label input");function o(e){var t=e.currentTarget.value;localStorage.setItem("feedback - form - state",t),console.log(t)}console.log(e),console.log(t),e.addEventListener("input",o),t.addEventListener("input",o)}();
+//# sourceMappingURL=03-feedback.71cc3f04.js.map
