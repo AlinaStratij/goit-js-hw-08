7b65206af89cd2c3fdea352bee444e95ba246800
AlinaStratij <99261910+AlinaStratij@users.noreply.github.com>
2022-09-02 23:42:37 +0000

    Deploying to gh-pages from @ AlinaStratij/goit-js-hw-08@aa974c77a710239bc911971490be90a4c4004ac7 🚀

--- 03-feedback.fa7a168f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form textarea"),t=document.querySelector(".feedback-form label input");function o(e){const t=e.currentTarget.value;localStorage.setItem("feedback - form - state",t),console.log(t)}console.log(e),console.log(t),e.addEventListener("input",o),t.addEventListener("input",o);
-//# sourceMappingURL=03-feedback.fa7a168f.js.map
