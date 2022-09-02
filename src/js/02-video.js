aa974c77a710239bc911971490be90a4c4004ac7
AlinaStratij <astratijcuk@gmail.com>
2022-09-03 02:42:13 +0300

    task 3 changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,10 @@
- import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+const iframe = document.querySelector(`iframe`);
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  // data is an object containing properties specific to that event
+});
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,33 @@
-const textarea = document.querySelector(`.feedback-form textarea`);
-const input = document.querySelector(`.feedback-form label input`);
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(`.feedback-form`);
+console.log(form) 
+
+const textarea = document.querySelector(`.feedback-form textarea`)
 console.log(textarea)
-console.log(input)
-textarea.addEventListener(`input`, OnFormInput)
-input.addEventListener(`input`, OnFormInput);
-function OnFormInput(evt) {
-    const message = evt.currentTarget.value;
-    localStorage.setItem(`feedback - form - state`, message);
-    console.log(message)
+
+form.addEventListener(`submit`, onFormSubmit)
+textarea.addEventListener(`input`, throttle(onTextareaInput,500));
+
+const STORAGE_KEY=`feedback-form-state`;
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();  
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
 }
+ function onTextareaInput(evt) {
+     const message = evt.target.value;
+     localStorage.setItem(STORAGE_KEY, message);
+ }
+
+function populateTextarea(){
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        console.log(savedMessage);
+        textarea.value = savedMessage;
+    }
+ }
