4df1e42606c4647906109e118a4e0987ba2088e8
AlinaStratij <astratijcuk@gmail.com>
2022-09-08 16:08:27 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,44 +1,21 @@
+
 import Player from '@vimeo/player';
-const iframe = document.querySelector(`iframe`);
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  // data is an object containing properties specific to that event
-});
+const STORAGE_KEY='videoplayer-current-time';
+
+function onTimeUpDate(date) {
+    const storageDate = localStorage.setItem(STORAGE_KEY, date.seconds));
+    console.log(storageDate);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-
-// const localStorageKey = 'videoplayer-current-time';
-
-// function onTimeUpDate(evt) {
-//   localStorage.setItem(localStorageKey, evt.seconds);
-// }
-// const saveTime = localStorage.getItem(localStorageKey);
-// console.log(saveTime);
-// if (saveTime) {
-//   player.setCurrentTime(saveTime);
-// }
-// player.on('timeupdate', throttle(onTimeUpDate, 1000));
+const saveTime = Math.round(localStorage.getItem(STORAGE_KEY));
+console.log(saveTime);
+if (saveTime) {
+  player.setCurrentTime(saveTime);
+}
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
